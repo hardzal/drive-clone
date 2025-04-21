ee701c26231b37b15d9e569e6434801f4c0aae25
hardzal <suryadijogja@gmail.com>
2025-04-22 06:47:40 +0700

    Add themeprovider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { DriveUI } from "~/components/drive-ui"
-import { ThemeProvider } from "~/components/theme-provider"
+import { DriveUI } from "~/components/drive-ui";
+import { ThemeProvider } from "~/components/theme-provider";
 
 export default function Home() {
   return (
     <ThemeProvider defaultTheme="dark" attribute="class">
       <DriveUI />
     </ThemeProvider>
-  )
+  );
 }
